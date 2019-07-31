9ed78262b77bb590a39ed7a77dd061ecde9c4f35
kim3040 <53500701+kim3040@users.noreply.github.com>
2019-07-30 23:48:30 -0300

    Create comandos.py

---
+++ comandos.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import amanobot
+from amanobot.loop import MessageLoop
+import time
+
+bot = amanobot.Bot('SEUTOKEN')
+
+def handle(msg):
+ if msg['text'] == '/start':
+  bot.sendMessage(msg['chat']['id'], 'Olá')
+# toda vez que um usuário digitar /start uma mensagem sera enviada para o mesmo no caso a mensagem enviada sera Olá
+ elif msg['text'] == '/help':
+  bot.sendMessage(msg['chat']['id'], 'em que posso ajudar')
+  
+
+MessageLoop(bot, handle).run_as_thread()
+while 1:
+  time.sleep(10)
